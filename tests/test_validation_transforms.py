b26fc684f14fc0879e953340c1068f64c7c51bee
Javier Carnero <javier@carnero.net>
2023-03-04 22:00:03 +0100

    tested complete transforms pipeline

--- tests/test_validation_transforms.py
+++ tests/test_validation_transforms.py
@@ -65,18 +65,45 @@ class TestTransforms:
         ).all()
 
     def testFlipEqualsCustomGPUCrop(self, im_fastai: PILImage, im_pil: Image):
+        # apply flip augmentation on validation
         tt_fastai = fastai_aug.ToTensor()
         i2f_fastai = fastai_aug.IntToFloatTensor()
         flip = fastai_aug.Flip(size=(224, 224))
-        tt_torch = tvtfms.ToTensor()
-
-        # apply flip augmentation on validation
         result_im_fastai = flip(
             i2f_fastai(tt_fastai(im_fastai).unsqueeze(0)), split_idx=1
         )
 
         # apply custom gpu crop
+        tt_torch = tvtfms.ToTensor()
         result_im_tv = gpu_crop(tt_torch(im_pil).unsqueeze(0), size=(224, 224))
+
+        assert torch.allclose(result_im_fastai, result_im_tv)
+        assert (result_im_fastai == result_im_tv).all()
+
+    def testFastaiTransformsEqualsCustom(self, im_fastai: PILImage, im_pil: Image):
+        # fastai transforms
+        crop_rrc = fastai_aug.RandomResizedCrop((460, 460))
+        tt_fastai = fastai_aug.ToTensor()
+        i2f_fastai = fastai_aug.IntToFloatTensor()
+        flip = fastai_aug.Flip(size=(224, 224))
+        brightness = fastai_aug.Brightness()
+        norm_fastai = fastai_aug.Normalize.from_stats(
+            *fastai_aug.imagenet_stats, cuda=False
+        )
+
+        # custom transforms
+        tt_torch = tvtfms.ToTensor()
+        norm_torch = tvtfms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+
+        # apply all fastai augmentations on validation (transformations)
+        batch_im_fastai = tt_fastai(crop_rrc(im_fastai, split_idx=1)).unsqueeze(0)
+        result_im_fastai = norm_fastai(
+            brightness(flip(i2f_fastai(batch_im_fastai), split_idx=1), split_idx=1)
+        )
+
+        # apply all custom transformations
+        batch_im_tv = tt_torch(resized_crop_pad(im_pil, (460, 460))).unsqueeze(0)
+        result_im_tv = norm_torch(gpu_crop(batch_im_tv, size=(224, 224)))
 
         assert torch.allclose(result_im_fastai, result_im_tv)
         assert (result_im_fastai == result_im_tv).all()
