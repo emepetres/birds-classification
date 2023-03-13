916ab8ebf56e44fd33b2c204966b35c72a1ad616
Javier Carnero <javier@carnero.net>
2023-03-13 21:57:21 +0000

    Copy deployment files to gradio branch

--- training/birds/config.py
+++
@@ -1,6 +0,0 @@
-DATA_STORAGE_PATH = "../data"
-MODELS_STORAGE_PATH = "../models"
-DATASET = "200-bird-species-with-11788-images"
-OWNER = "veeralakrishna"
-
-DATA_PATH = DATA_STORAGE_PATH + "/" + DATASET
